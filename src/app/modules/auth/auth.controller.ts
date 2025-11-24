78c8d2749bd175c292dd799434a7e10d8be9c7ff
unknown <najimuddinhelalal670@gmail.com>
2025-11-24 10:07:05 +0600

    added domain for clear cookie

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -26,6 +26,8 @@ const logout = catchAsync(
       httpOnly: true,
       secure: true,
       sameSite: "none",
+      domain: "ride-booking-management-system.vercel.app",
+      path: "/",
     });
     sendResponse(res, {
       success: true,
